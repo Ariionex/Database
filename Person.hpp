18457fefb6bc417375ee55c91d06372489298ba2
Ariionex <47718240+Ariionex@users.noreply.github.com>
2019-02-17 16:59:55 +0100

    Create Person.hpp

---
+++ Person.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+enum class Gender : char {male='m', female='f'};
+
+class Person
+{
+public:
+    Person(const string & name,
+           const string & lastname,
+           const string & address,
+           const string & pesel,
+           const Gender gender);
+        string getLastname() const;
+        string getPesel() const;
+        virtual int getIndex() const;
+
+private:
+    string name_;
+    string lastname_;
+    string address_;
+    string pesel_;
+    Gender gender_;
+};

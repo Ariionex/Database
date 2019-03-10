16aaaec401e1798d48055c09508b2a98515254d5
Ariionex <47718240+Ariionex@users.noreply.github.com>
2019-03-10 10:54:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,20 @@ int main()
     db.addStudent(jan);
     Person* adam = new Student("Adam","Adamski","Dziwna 10","0987654321",Gender::male,222222);
     db.addStudent(adam);
+    Person* krzysztof = new Student("Krzysztof","Jarzyna","Szczecinska 11","1987654320",Gender::male,333222);
+    db.addStudent(krzysztof);
+    Person* lucyfer = new Student("Lucyfer","Podolski","Szczesliwej 666","8887654320",Gender::male,333444);
+    db.addStudent(lucyfer);
+    Person* baltazar = new Student("Baltazar","Smith","Kowalowa 77","2987654320",Gender::male,336722);
+    db.addStudent(baltazar);
     db.sortByIndex();
-    db.removeStudent(111111);
-    db.removeStudent(222222);
+    
+    db.show();
+    cout<<endl;
+    db.removeByPesel("8887654320");
+    db.sortByLastname();
+    db.save("Baza_danych.txt");
+    db.load("Baza_danych.txt");
+    db.show();
     return 0;
 }
